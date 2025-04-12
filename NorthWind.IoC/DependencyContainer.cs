8528d0df503d513bc1ef3c1a4450a570ea2a7e9a
mistercristian <cristianguzbo@gmail.com>
2025-04-12 15:33:49 -0500

    Wind

---
+++ NorthWind.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NorthWind.Writers;
+using NorthWindEntities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependecyInjection;
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddNorthWindServices(this IServiceCollection services)
+            { 
+                services.AddDebugWriter();
+                services.AddServices();
+        
+                return services;
+            }
+}
